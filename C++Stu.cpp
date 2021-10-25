109a47691e8c9cfa63c2d876a4b782b0fcc37185
Golds <goldslee@foxmail.com>
2021-10-25 20:31:01 +0800

    plus

---
+++ C++Stu.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int abc(int a, int b, int c);
+
+int main()
+{
+	cout << "Hello World!\n";
+	cout << abc(1, 2, 4);
+}
+
+int abc(int a, int b, int c) {
+	return a + b * c;
+}
+
+float abc(float a, float b, float c) {
+	return a + b * c;
+}
+
+template<class T>
+T abc(T a, T b, T c) {
+	return a + b * c;
+}
