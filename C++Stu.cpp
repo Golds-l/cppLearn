4f56536a20f0abaaf441bd6256283620d46a9cae
Golds <goldslee@foxmail.com>
2021-10-25 21:35:43 +0800

    T& a, T& b, T& c

--- C++Stu.cpp
+++ C++Stu.cpp
@@ -2,14 +2,6 @@
 
 using namespace std;
 
-int abc(int a, int b, int c);
-
-int main()
-{
-	cout << "Hello World!\n";
-	cout << abc(1, 2, 4);
-}
-
 int abc(int a, int b, int c) {
 	return a + b * c;
 }
@@ -21,4 +13,29 @@ float abc(float a, float b, float c) {
 template<class T>
 T abc(T a, T b, T c) {
 	return a + b * c;
+}
+
+template<class T>
+T abc(T& a, T& b, T& c) {
+	return a + b * c;
+}
+
+template<class T>
+T abc(const T& a, const T& b, const T& c) {
+	return a + b * c;
+}
+
+template<class Ta, class Tb, class Tc>
+Ta abc(const Ta& a, const Tb& b, const Tc& c) {
+	return a + b * c;
+}
+
+int main()
+{
+	cout << "Hello World!\n";
+	//cout << abc(1, 2, 4);
+	char a[] = { 'a', 'b', 'c' };
+	for (int i = 0; i < 3; i++) {
+		cout << a[i];
+	}
 }
